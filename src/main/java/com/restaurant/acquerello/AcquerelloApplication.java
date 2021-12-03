df4c5b6cecb19741c36463dfd5280917b2e48e04
Brito Maria Luz <britomluz@gmail.com>
2021-12-03 10:59:52 -0300

    start proyect

---
+++ src/main/java/com/restaurant/acquerello/AcquerelloApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.acquerello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcquerelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcquerelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restaurant/acquerello/AcquerelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.acquerello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcquerelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
